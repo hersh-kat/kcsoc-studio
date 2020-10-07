e2cf07facd24b26607ef2434841b1a0dc46b585f
Josh Guha <joshguha@gmail.com>
2020-10-07 15:45:09 +0100

    Added form validation

--- src/components/ui/Login.js
+++ src/components/ui/Login.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, TextField, Button, Typography } from "@material-ui/core";
 
 export default function Login() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [helperMessages, setHelperMessages] = useState({
+		username: "",
+		password: "",
+	});
+	const [errors, setErrors] = useState({
+		username: false,
+		password: false,
+	});
+	const [submitDisabled, setSubmitDisabled] = useState(false);
+
+	const submit = (e) => {
+		e.preventDefault();
+		console.log(username, password);
+		if (!username && !password) {
+			setHelperMessages({
+				username: "Username is required",
+				password: "Password is required",
+			});
+			setErrors({
+				username: true,
+				password: true,
+			});
+			return;
+		}
+		if (!username) {
+			setHelperMessages({
+				username: "Username is required",
+			});
+			setErrors({
+				username: true,
+			});
+			return;
+		}
+		if (!password) {
+			setHelperMessages({
+				password: "Password is required",
+			});
+			setErrors({
+				password: true,
+			});
+			return;
+		}
+		setErrors({ username: false, password: false });
+		setHelperMessages({ username: "", password: "" });
+		setSubmitDisabled(true);
+	};
 	return (
 		<Grid
 			container
@@ -29,15 +77,38 @@ export default function Login() {
 				justify="center"
 				style={{ paddingTop: "60px" }}
 			>
-				<TextField required id="filled-required" label="Username" />
+				<TextField
+					id="filled-required"
+					label="Username"
+					onChange={(e) => {
+						return setUsername(e.target.value);
+					}}
+					helperText={helperMessages.username}
+					error={errors.username}
+				/>
 			</Grid>
 
 			<Grid item style={{ paddingTop: "60px" }}>
-				<TextField required id="filled-required" label="Password" />
+				<TextField
+					id="filled-password-input"
+					label="Password"
+					type="password"
+					autoComplete="current-password"
+					onChange={(e) => {
+						return setPassword(e.target.value);
+					}}
+					helperText={helperMessages.password}
+					error={errors.password}
+				/>
 			</Grid>
 
 			<Grid item style={{ paddingTop: "60px" }}>
-				<Button variant="contained" color="primary">
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={submit}
+					disabled={submitDisabled}
+				>
 					Login
 				</Button>
 			</Grid>
